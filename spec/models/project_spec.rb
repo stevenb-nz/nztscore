cc871c5cfaa10f5886955dedf07e9d34efe24024
Steven Brown <sgbrown@mac.com>
2018-03-26 23:18:21 +1300

    first test

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe Tournament do
+  it "considers a new tournament to not be finished" do
+    tournament = Tournament.new
+    expect (tournament.finished?).not_to be_truthy
+  end
+
+end
